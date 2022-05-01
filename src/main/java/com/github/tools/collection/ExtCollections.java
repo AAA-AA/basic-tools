97eeddd82eb68c81c73a81b21fe407fef981b6cf
renhongqiang <820941512@qq.com>
2022-05-02 00:21:54 +0800

    -a
    增加集合运算方法

--- src/main/java/com/github/tools/collection/ExtCollections.java
+++ src/main/java/com/github/tools/collection/ExtCollections.java
@@ -4,12 +4,9 @@ import com.github.tools.pub.Checks;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public final class ExtCollections {
     public ExtCollections() {
@@ -70,5 +67,101 @@ public final class ExtCollections {
         return null == price ? BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP) : price.setScale(2, RoundingMode.HALF_UP);
     }
 
+    public static <E> List<E> asList(List<E> ...elements) {
+        List<E> list = new ArrayList<>();
+        for (List<E> element : elements) {
+            list.addAll(element);
+        }
+        return list;
+    }
+
+    public static <K,V> Map<K,V> asMap(K k, V val) {
+        HashMap<K,V> map = new HashMap<>();
+        map.put(k, val);
+        return map;
+    }
+
+    public static <E> CollectionAgg<E> join(Collection<E> left, Collection<E> right) {
+        CollectionAgg agg = new CollectionAgg();
+        if (left == null || left.isEmpty()) {
+            agg.setRightPart(right);
+            agg.setUnionPart(right);
+            return agg;
+        }
+        if (right == null || right.isEmpty()) {
+            agg.setLeftPart(left);
+            agg.setUnionPart(left);
+            return agg;
+        }
+        Collection<E> common = new HashSet<>();
+        for (E leftEle : left) {
+            for (E rightEle : right) {
+                if (leftEle.equals(rightEle)) {
+                    common.add(leftEle);
+                }
+            }
+        }
+        agg.setCommonPart(common);
+        Collection<E> leftPart = left.stream().filter(e -> !common.contains(e)).collect(Collectors.toSet());
+        Collection<E> rightPart = right.stream().filter(e -> !common.contains(e)).collect(Collectors.toSet());
+
+        agg.setLeftPart(leftPart);
+        agg.setRightPart(rightPart);
+        agg.setUnionPart(getUnion(leftPart, rightPart, common));
+        return agg;
+    }
+
+    private static <E> Collection<E> getUnion(Collection<E> leftPart, Collection<E> rightPart, Collection<E> common) {
+        Collection<E> union = new HashSet<>();
+        if (leftPart != null && !leftPart.isEmpty()) {
+            union.addAll(leftPart);
+        }
+        if (rightPart != null && !rightPart.isEmpty()) {
+            union.addAll(rightPart);
+        }
+        if (common != null && !common.isEmpty()) {
+            union.addAll(common);
+        }
+        return union;
+    }
+
+    public static class CollectionAgg<E> {
+        private Collection<E> leftPart;//补集左边部分
+        private Collection<E> rightPart;//补集右边部分
+        private Collection<E> commonPart;//交集
+        private Collection<E> unionPart;//并集
+
+        public Collection<E> getLeftPart() {
+            return leftPart;
+        }
+
+        public void setLeftPart(Collection<E> leftPart) {
+            this.leftPart = leftPart;
+        }
+
+        public Collection<E> getRightPart() {
+            return rightPart;
+        }
+
+        public void setRightPart(Collection<E> rightPart) {
+            this.rightPart = rightPart;
+        }
+
+        public Collection<E> getCommonPart() {
+            return commonPart;
+        }
+
+        public void setCommonPart(Collection<E> commonPart) {
+            this.commonPart = commonPart;
+        }
+
+        public Collection<E> getUnionPart() {
+            return unionPart;
+        }
+
+        public void setUnionPart(Collection<E> unionPart) {
+            this.unionPart = unionPart;
+        }
+    }
 }
 
